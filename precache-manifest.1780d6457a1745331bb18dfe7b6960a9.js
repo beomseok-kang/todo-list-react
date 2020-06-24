27bdf0c419f6700a9b08b590415da30eda92ad56
bumsuk980119 <beom.kang17@gmail.com>
2020-06-24 18:21:38 +0900

    Updates

---
+++ precache-manifest.1780d6457a1745331bb18dfe7b6960a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa2c1178738e3549a1455b3d1a94a7a",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "d8ec07dd65b82adbdb7c",
+    "url": "/todo-list-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5d982c22fc64c0cc7f56",
+    "url": "/todo-list-react/static/js/2.0d127b54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list-react/static/js/2.0d127b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ec07dd65b82adbdb7c",
+    "url": "/todo-list-react/static/js/main.962251e0.chunk.js"
+  },
+  {
+    "revision": "826642a5112903c57c77",
+    "url": "/todo-list-react/static/js/runtime-main.22aeb5a4.js"
+  }
+]);
